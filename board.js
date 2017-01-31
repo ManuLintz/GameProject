f4c8bb64576ff685d4fbdac7dbe16facb2e32e83
Manuel Lintz <manuel.lintz@gmail.com>
2017-01-31 11:45:04 +0100

    tuesday commit

--- board.js
+++ board.js
@@ -10,7 +10,7 @@ function Board(row,col,level){
 Board.prototype.buildBoard = function(){
   for (var boardHeight=0; boardHeight < this.col; boardHeight++){
     for (var boardWidth=0; boardWidth < this.row; boardWidth++){
-      $('.container').append('<li class="square" row="'+boardHeight+'" col="'+boardWidth+'"></li>');
+      $('.board-container').append('<li class="square" row="'+boardHeight+'" col="'+boardWidth+'"></li>');
     }
   }
 };
--- main.js
+++ main.js
@@ -1,59 +1,118 @@
 var start = false;
+function gameOver(){
+  alert ('Game over!!!');
+}
 
 function drawPath(){
   var column= fill.position.col;
   var row= fill.position.row;
   $('li[col='+ column+'][row='+row+']').addClass('wire');
 }
-$(document).ready(function(){
-  $('.square').on('click', function() {
-    var that = this;
-    if(start===false){
-      start=true;
-      $(that).addClass('wire');
-     fill.getPosition(that);
-   }
-   });
 
 
 
-   $("body").keydown(function(){
-     if(fill.pathUp()||fill.pathRight()||fill.pathDown()||fill.pathLeft()){
-     switch (event.keyCode) {
-          case 38:
-        fill.moveUp();
-        drawPath();
-       console.log('up!');
-         break;
+/*function resetClockColor(){
+    $('.clock').removeClass('run');
+    clockColor();
 
-         case 40:
-        fill.moveDown();
-        drawPath();
-        console.log('down!');
-         break;
+}
 
-         case 37:
-        fill.moveLeft();
-        drawPath();
-       console.log('left!');
-         break;
+function clockColor(){
+  $('.clock').addClass('run');
+}*/
+////////////////////////////////////////////
+var playerPlayed = false;
+var a;
+var test= 100;
 
-        case 39:
-        fill.moveRight();
-        drawPath();
-       console.log('right!');
-        break;}}
+function res(){
+  clearInterval(a);
+  test=100;
+}
 
-        else{
-          alert("Game Over Bitch");
+a=setInterval (function(){
+  console.log(test);
+  if (test===0){
+    clearInterval(a);
+  }
+  test--;
+},1000);
+
+
+////////////////////////////////////////////
+var gameClock;
+
+function gameEnd(){
+   gameClock=setTimeout (function(){
+
+     alert ('Game over!!!');
+},5000);}
+/////////////////////////////////////////////
+function game(){
+$('.square').on('click', function() {
+  var that = this;
+  if(start===false){
+    start=true;
+    $(that).addClass('wire');
+   fill.getPosition(that);
+   gameEnd();
+ }
+ });
+
+
+$("body").keydown(function(){
+   if(fill.pathUp()||fill.pathRight()||fill.pathDown()||fill.pathLeft()){
+   switch (event.keyCode) {
+        case 38:
+      fill.moveUp();
+      drawPath();
+       clearTimeout( gameClock );
+       gameEnd();
+       $('.clock').addClass('run');
+     console.log('up!');
+       break;
+
+       case 40:
+      fill.moveDown();
+      drawPath();
+      clearTimeout( gameClock );
+      gameEnd();
+       $('.clock').addClass('run');
+      console.log('down!');
+       break;
+
+       case 37:
+      fill.moveLeft();
+      drawPath();
+      clearTimeout( gameClock );
+      gameEnd();
+    resetClockColor();
+     console.log('left!');
+       break;
+
+      case 39:
+      fill.moveRight();
+      drawPath();
+      clearTimeout( gameClock );
+      gameEnd();
+     console.log('right!');
+      break;}}
+
+      else{
+          var score = $('.wire').length;
+          if (score===98){alert("you win!");
+          $('.board-container').empty();
+          board.buildBoard();}
+          else{alert("Game Over Bitch");
+          $('.board-container').empty();
+          board.buildBoard();
+          return game();}
         }
+    });
+  }
+
+$(document).ready(function(){
 
 
-     });
-
-
-
-
-
-
-});
+    game();
+  });
